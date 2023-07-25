f16f1cda17fdbe7bcced8b050db6d84a9d1e935c
unknown <nicosetzes@gmail.com>
2023-07-25 14:51:41 -0300

    V1.8 I've added a script so specific links are target=_self on mobile but target=_blank on desktop.

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Hago que el target de ciertos enlaces NO sea _blank en viewports menores a 768px //
 
 if (window.matchMedia("(max-width: 768px)").matches) {
-  const links = document.querySelectorAll(".link-footer");
+  const links = document.querySelectorAll(".link-target");
   links.forEach((link) => {
     link.target = "_self";
   });
